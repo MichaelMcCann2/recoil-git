09996e6bc76936e17d2edb8e945eb2c523160a77
Michael McCann <michael.mccann@experoinc.com>
2021-11-24 15:20:14 -0600

    set up layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Input from './components/Input';
+import { taskListState } from './Recoil/TaskListState';
+import { useRecoilValue } from 'recoil';
+import Task from './components/Task';
 
 function App() {
+
+  const list = useRecoilValue(taskListState)
+
+  console.log(list)
+
   return (
     <div className="App">
       <button className="themeButton">Change Theme</button>
       <h1 className="title">Todo list</h1>
-      
+      <Input />
+
+      {list.map((item, index) => (
+        <Task key={index}/>
+      ))}
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Input() {
+  return (
+    <div className="input">
+      <input type="text"></input>
+      <button>Add Task</button>
+    </div>
+  )
+}
