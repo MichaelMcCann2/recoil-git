edb8c76c266886defc8762ea1cd5ee26c2edd218
Michael McCann <michael.mccann@experoinc.com>
2021-11-24 15:49:39 -0600

    add theme toggle functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import Input from './components/Input';
 import { taskListState } from './Recoil/TaskListState';
-import { useRecoilValue } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import Task from './components/Task';
+import { themeState } from './Recoil/ThemeState'
 
 function App() {
 
   const list = useRecoilValue(taskListState)
+  const theme = useRecoilValue(themeState)
+  const setThemeState = useSetRecoilState(themeState)
 
-  console.log(list)
+  const handleClick = function(){
+    if (theme === 'light') setThemeState('dark')
+    else setThemeState('light')
+  }
 
   return (
-    <div className="App">
-      <button className="themeButton">Change Theme</button>
+    <div className={`App ${theme === 'dark' ? 'dark' : ''}`}>
+      <button className="themeButton" onClick={handleClick}>Change Theme</button>
       <h1 className="title">Todo list</h1>
       <Input />
 
